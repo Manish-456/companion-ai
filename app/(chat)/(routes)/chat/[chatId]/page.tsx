07a5baa7f2e564020f95c8380cec5f72a590e6e8
Manish Tamang <111108242+Manish-456@users.noreply.github.com>
2023-10-26 18:23:10 +0545

    order fixed

--- app/(chat)/(routes)/chat/[chatId]/page.tsx
+++ app/(chat)/(routes)/chat/[chatId]/page.tsx
@@ -21,7 +21,7 @@ export default async function ChatIdPage({ params }: ChatIdPageProps) {
     include: {
       messages: {
         orderBy: {
-          createdAt: "desc",
+          createdAt: "asc",
         },
         where: {
           userId,
